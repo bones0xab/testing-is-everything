e9651e0a584ca6873c19c5cd3fca3b300d51e8d2
bones0xab <abdlkbir.dacosta12@gmail.com>
2025-11-12 13:39:21 +0100

    changement pof yml

--- src/main/java/com/example/testing/TestingApplication.java
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -2,12 +2,17 @@ package com.example.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class TestingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
+        System.out.println("We are running our application ");
 	}
+
+
+
 
 }
